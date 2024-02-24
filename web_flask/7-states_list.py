6606521b6c44bfe7ed3d412c27d6389889503731
r-iradukunda <r.iradukund@alustudent.com>
2024-02-24 11:41:10 -0800

    States

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""
-This is module 7-states_list.i
 
-In this module, we combine Flask with SQLAlchemy for the first time.
-Run this script from the AirBnB_v2 directory for imports.
-"""
-from os import getenv
-from flask import Flask, render_template
+"""Starts a Flask web application"""
+
 from models import storage
 from models.state import State
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_list/')
-def list_states():
-    """List all states in a database."""
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", Query_name="States", states=states)
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """Comment"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """Remove the session to see what happened."""
+def teardown(self):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5300")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
